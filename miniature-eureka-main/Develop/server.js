b1b56d43d6c8a8ba99f7e7d4aad71b92d673da86
Lady Godiva <watson.amber.denise@gmail.com>
2023-12-06 13:05:52 -0700

    updated file locations

--- miniature-eureka-main/Develop/server.js
+++ miniature-eureka-main/Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const routes = require('./miniature-eureka-main/Develop/routes/notes.js');
+const routes = require('./routes/notes.js');
 
 
 
